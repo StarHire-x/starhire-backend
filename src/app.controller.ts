2e4a7fcdb6ad3d2e244d5ed0a1471a95deab5275
Bryan Leong <bryanleongys@gmail.com>
2023-08-31 10:26:59 +0800

    added post req

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Post()
+  postHello(): string {
+    return this.appService.getHello();
+  }
 }

264cd3a95175e6f3a87e73cb300cb82befaff5ec
Deshun99 <e0560014@u.nus.edu>
2023-09-04 20:32:08 +0800

    added crud for document

--- src/document/document.module.ts
+++ src/document/document.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DocumentService } from './document.service';
 import { DocumentController } from './document.controller';
+import { Document } from 'src/entities/document.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Document])],
   controllers: [DocumentController],
   providers: [DocumentService],
 })
--- src/document/document.service.ts
+++ src/document/document.service.ts
@@ -1,26 +1,79 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateDocumentDto } from './dto/create-document.dto';
 import { UpdateDocumentDto } from './dto/update-document.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Document } from 'src/entities/document.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class DocumentService {
-  create(createDocumentDto: CreateDocumentDto) {
-    return 'This action adds a new document';
+  constructor(
+    @InjectRepository(Document)
+    private readonly documentRepository: Repository<Document>,
+  ) {}
+
+  async create(createDocumentDto: CreateDocumentDto) {
+    try {
+
+      const document = new Document({
+        ...createDocumentDto
+      });
+
+      return await this.documentRepository.save(document);
+    } catch (err) {
+      throw new HttpException(
+        'Failed to create new document',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 
-  findAll() {
-    return `This action returns all document`;
+  async findAll() {
+    return await this.documentRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} document`;
+  async findOne(id: number) {
+    try {
+      // For this part, u want the relationship with other entities to show, at most 1 level, no need too detail
+      return await this.documentRepository.findOne({
+        where: { documentId: id },
+        relations: {},
+      });
+    } catch (err) {
+      throw new HttpException(
+        'Failed to find document',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 
-  update(id: number, updateDocumentDto: UpdateDocumentDto) {
-    return `This action updates a #${id} document`;
+  async update(id: number, updateDocumentDto: UpdateDocumentDto) {
+    try {
+      const document = await this.documentRepository.findOneBy({
+        documentId: id,
+      });
+
+      Object.assign(document, updateDocumentDto);
+      return await this.documentRepository.save(document);
+
+    } catch (err) {
+      throw new HttpException(
+        'Failed to update document',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} document`;
+  async remove(id: number) {
+    try {
+      return await this.documentRepository.delete({
+        documentId: id,
+      });
+    } catch (err) {
+      throw new HttpException(
+        'Failed to delete document',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 }

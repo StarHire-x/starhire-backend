83c31fbc8310f07a1ffff0574e835b24d826ce87
Deshun99 <e0560014@u.nus.edu>
2023-09-05 20:30:48 +0800

    job seeker job preference modified

--- src/job-preference/dto/create-job-preference.dto.ts
+++ src/job-preference/dto/create-job-preference.dto.ts
@@ -1,3 +1,10 @@
-import { JobPreferenceDetailDto } from './job-preference-detail.dto';
+import { CreateJobSeekerDto } from "src/job-seeker/dto/create-job-seeker.dto";
 
-export class CreateJobPreferenceDto extends JobPreferenceDetailDto {}
+export class CreateJobPreferenceDto {
+  locationPreference: number;
+  salaryPreference: number;
+  culturePreference: number;
+  diversityPreference: number;
+  workLifeBalancePreference: number;
+  jobSeeker: CreateJobSeekerDto;
+}
--- src/job-preference/job-preference.service.ts
+++ src/job-preference/job-preference.service.ts
@@ -1,64 +1,93 @@
 import {
   ConflictException,
+  HttpException,
+  HttpStatus,
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
 import { CreateJobPreferenceDto } from './dto/create-job-preference.dto';
 import { UpdateJobPreferenceDto } from './dto/update-job-preference.dto';
-import { JobPreferenceRepo } from './job-preference.repo';
+import { JobPreference } from 'src/entities/jobPreference.entity';
+import { JobSeeker } from 'src/entities/jobSeeker.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { JobSeekerService } from 'src/job-seeker/job-seeker.service';
 
 @Injectable()
 export class JobPreferenceService {
-  constructor(private readonly jobPreferenceRepo: JobPreferenceRepo) {}
+  constructor(
+    @InjectRepository(JobPreference)
+    private readonly jobPreferenceRepository: Repository<JobPreference>,
+    @InjectRepository(JobSeeker)
+    private readonly jobSeekerRepository: Repository<JobSeeker>,
+  ) {}
 
-  async create(createJobPreference: CreateJobPreferenceDto) {
+  async create(
+    jobSeekerId: number,
+    createJobPreference: CreateJobPreferenceDto,
+  ) {
     try {
-      const { ...jobPreferenceDetail } = createJobPreference;
-      return await this.jobPreferenceRepo.createJobPreference(
-        jobPreferenceDetail,
-      );
+      const { jobSeeker, ...dtoExcludeRelationship } = createJobPreference;
+      const findJobSeeker = await this.jobSeekerRepository.findOneBy({ userId: jobSeekerId });
+      if (!findJobSeeker) {
+        throw new NotFoundException('Job Seeker Id provided is not valid');
+      }
+      const jobPreference = new JobPreference({
+        ...dtoExcludeRelationship,
+        jobSeeker: findJobSeeker,
+      });
+      return await this.jobPreferenceRepository.save(jobPreference);
     } catch (err) {
-      throw new ConflictException(
-        'Inserting a duplicate entry into the database. Please check your data.',
+      throw new HttpException(
+        'Failed to create new job preference',
+        HttpStatus.BAD_REQUEST,
       );
     }
   }
 
   async findAll() {
-    return await this.jobPreferenceRepo.findAllJobPreferences();
+    return await this.jobPreferenceRepository.find();
   }
 
   async findOne(id: number) {
-    const jobPreference = await this.jobPreferenceRepo.findOneJobPreference(id);
-    if (jobPreference === null) {
-      throw new NotFoundException(`Job Preference with ID ${id} not found`);
-    } else {
-      return jobPreference;
+    try {
+      return await this.jobPreferenceRepository.findOne({
+        where: { jobPreferenceId: id },
+        relations: { jobSeeker: true },
+      });
+    } catch (err) {
+      throw new HttpException(
+        'Failed to find job preference',
+        HttpStatus.BAD_REQUEST,
+      );
     }
   }
 
   async update(id: number, updateJobPreference: UpdateJobPreferenceDto) {
-    const jobPreference = await this.jobPreferenceRepo.findOneJobPreference(id);
-    if (jobPreference === null) {
-      throw new NotFoundException(
-        `Job Preference with ID ${id} not found, Update Unsuccessful`,
-      );
-    } else {
-      return await this.jobPreferenceRepo.updateJobPreference(
-        id,
-        updateJobPreference,
-      );
+    // Ensure valid job listing Id is provided
+    const jobListing = await this.findOne(id);
+    if (!jobListing) {
+      throw new NotFoundException('Job Listing Id provided is not valid');
     }
+    const { jobSeeker, ...dtoExcludeRelationship } = updateJobPreference;
+    Object.assign(jobListing, dtoExcludeRelationship);
+    return await this.jobPreferenceRepository.save(jobListing);
+  }
+  catch(err) {
+    throw new HttpException(
+      'Failed to update job preference',
+      HttpStatus.BAD_REQUEST,
+    );
   }
 
   async remove(id: number) {
-    const jobPreference = await this.jobPreferenceRepo.findOneJobPreference(id);
-    if (jobPreference === null) {
-      throw new NotFoundException(
-        `Job Preference with ID ${id} not found, Delete Unsuccessful`,
+    try {
+      return await this.jobPreferenceRepository.delete({ jobPreferenceId: id });
+    } catch (err) {
+      throw new HttpException(
+        'Failed to delete job preference',
+        HttpStatus.BAD_REQUEST,
       );
-    } else {
-      return await this.jobPreferenceRepo.deleteJobPreference(id);
     }
   }
 }

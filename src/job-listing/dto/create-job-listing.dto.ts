9de675954a317fb770c70555a1b0272c2a25f476
rilwan99 <rilwanwild@gmail.com>
2023-09-05 17:37:02 +0800

    update job listing service class

--- src/job-listing/dto/create-job-listing.dto.ts
+++ src/job-listing/dto/create-job-listing.dto.ts
@@ -1,14 +1,14 @@
-import JobListingStatusEnum from "src/enums/jobListingStatus.enum";
-import { CreateJobApplicationDto } from "src/job-application/dto/create-job-application.dto";
+import JobListingStatusEnum from 'src/enums/jobListingStatus.enum';
+import { CreateJobApplicationDto } from 'src/job-application/dto/create-job-application.dto';
 
 export class CreateJobListingDto {
-    jobListingId: number;
-    title: string;
-    description: string;
-    jobLocation: string;
-    listingDate: Date;
-    averageSalary: number;
-    jobStartDate: Date;
-    jobListingStatus: JobListingStatusEnum;
-    jobApplications: CreateJobApplicationDto[];
+  jobListingId: number;
+  title: string;
+  description: string;
+  jobLocation: string;
+  listingDate: Date;
+  averageSalary: number;
+  jobStartDate: Date;
+  jobListingStatus: JobListingStatusEnum;
+  jobApplications: CreateJobApplicationDto[];
 }

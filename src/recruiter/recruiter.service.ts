c2467c5e0c38aad00d2eadb6bcd50f622df31a3f
Lukas Lee <jiawei3e1@gmail.com>
2023-09-07 19:51:32 +0800

    CRUD for recruiter done

--- src/recruiter/recruiter.service.ts
+++ src/recruiter/recruiter.service.ts
@@ -3,36 +3,122 @@ import { CreateRecruiterDto } from './dto/create-recruiter.dto';
 import { UpdateRecruiterDto } from './dto/update-recruiter.dto';
 import {
   ConflictException,
+  HttpException,
+  HttpStatus,
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
 import { QueryFailedError, Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import NotificationModeEnum from 'src/enums/notificationMode.enum';
+import UserStatusEnum from 'src/enums/userStatus.enum';
+import UserRoleEnum from 'src/enums/userRole.enum';
 
 @Injectable()
 export class RecruiterService {
   constructor(
     @InjectRepository(Recruiter)
-    private readonly recruiterRepo: Repository<Recruiter>,
+    private readonly recruiterRepository: Repository<Recruiter>,
   ) {}
 
   async create(createRecruiterDto: CreateRecruiterDto) {
-    
+    try {
+      const recruiter = new Recruiter({ ...createRecruiterDto });
+
+      // Convert all ENUM values
+      if (recruiter.status) {
+        recruiter.status = this.mapStatusToEnum(recruiter.status);
+      }
+      if (recruiter.notificationMode) {
+        recruiter.notificationMode = this.mapNotificationToEnum(
+          recruiter.notificationMode,
+        );
+      }
+      if (recruiter.role) {
+        recruiter.role = this.mapRoleToEnum(recruiter.role);
+      }
+      return await this.recruiterRepository.save(recruiter);
+    } catch (err) {
+      throw new HttpException(err.message, HttpStatus.BAD_REQUEST);
+    }
   }
 
   async findAll() {
-    
+    return await this.recruiterRepository.find();
   }
 
   async findOne(id: number) {
-    
+    try {
+      const recruiter = await this.recruiterRepository.findOne({
+        where: { userId: id },
+        relations: {
+          commissions: true,
+          jobApplications: true,
+          chats: true,
+          tickets: true,
+        },
+      });
+      return recruiter;
+    } catch (err) {
+      throw new HttpException(err.message, HttpStatus.BAD_REQUEST);
+    }
   }
 
-  async update(id: number, updateRecruiterDto: UpdateRecruiterDto) {
-    
+  async update(id: number, updatedRecruiter: UpdateRecruiterDto) {
+    try {
+      const recruiter = await this.recruiterRepository.findOneBy({
+        userId: id,
+      });
+
+      if (!recruiter) {
+        throw new NotFoundException('Recruiter User Id provided is not valid');
+      }
+
+      Object.assign(recruiter, updatedRecruiter);
+
+      return await this.recruiterRepository.save(recruiter);
+    } catch (err) {
+      throw new HttpException(err.message, HttpStatus.BAD_REQUEST);
+    }
   }
 
   async remove(id: number) {
-    
+    try {
+      return await this.recruiterRepository.delete({ userId: id });
+    } catch (err) {
+      throw new HttpException(
+        'Failed to delete a recruiter',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  mapNotificationToEnum(status: string): NotificationModeEnum {
+    switch (status.toLowerCase()) {
+      case 'sms':
+        return NotificationModeEnum.SMS;
+      default:
+        return NotificationModeEnum.EMAIL;
+    }
+  }
+  mapStatusToEnum(status: string): UserStatusEnum {
+    switch (status.toLowerCase()) {
+      case 'inactive':
+        return UserStatusEnum.INACTIVE;
+      default:
+        return UserStatusEnum.ACTIVE;
+    }
+  }
+  mapRoleToEnum(status: string): UserRoleEnum {
+    switch (status.toLowerCase()) {
+      case 'recruiter':
+        return UserRoleEnum.RECRUITER;
+      case 'corporate':
+        return UserRoleEnum.CORPORATE;
+      case 'administrator':
+        return UserRoleEnum.ADMINISTRATOR;
+      default:
+        return UserRoleEnum.JOBSEEKER;
+    }
   }
 }

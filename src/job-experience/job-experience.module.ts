a7d7974bb3b14c010a6bb395a6610dc98426d347
rilwan99 <rilwanwild@gmail.com>
2023-09-25 12:32:02 +0800

    include imports in job experience module

--- src/job-experience/job-experience.module.ts
+++ src/job-experience/job-experience.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { JobExperienceService } from './job-experience.service';
 import { JobExperienceController } from './job-experience.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { JobExperience } from 'src/entities/jobExperience';
+import { JobSeeker } from 'src/entities/jobSeeker.entity';
+import { JobSeekerService } from 'src/job-seeker/job-seeker.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([JobExperience, JobSeeker])],
   controllers: [JobExperienceController],
-  providers: [JobExperienceService],
+  providers: [JobExperienceService, JobSeekerService],
 })
 export class JobExperienceModule {}

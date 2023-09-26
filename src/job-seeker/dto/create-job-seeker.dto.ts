322dec193c8cb8cb255f039d401e81494218eae2
Deshun99 <e0560014@u.nus.edu>
2023-09-26 20:00:33 +0800

    added visibility enum

--- src/job-seeker/dto/create-job-seeker.dto.ts
+++ src/job-seeker/dto/create-job-seeker.dto.ts
@@ -1,6 +1,7 @@
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import HighestEducationStatusEnum from 'src/enums/highestEducationStatus.enum';
 import { PartialType } from '@nestjs/mapped-types';
+import VisibilityEnum from 'src/enums/visibility.enum';
 
 export class CreateJobSeekerDto extends PartialType(CreateUserDto) {
   resumePdf: string;
@@ -9,4 +10,7 @@ export class CreateJobSeekerDto extends PartialType(CreateUserDto) {
   highestEducationStatus: HighestEducationStatusEnum;
   profilePictureUrl: string;
   homeAddress: string;
+  instituteName: string;
+  dateOfGraduation: Date;
+  visibility: VisibilityEnum;
 }

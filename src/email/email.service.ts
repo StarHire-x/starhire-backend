a54585174745f0c38331477f9af91c2623cdc7ed
Alwin Ng Jun Wei <alwinngjw@gmail.com>
2023-09-28 16:13:19 +0800

    formatted email service

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -8,24 +8,23 @@ export class EmailService {
 
   async resetPassword(createEmailDto: CreateEmailDto) {
     try {
-        await this.mailerService.sendMail({
-          to: createEmailDto.emailAddress,
-          subject: 'Reset Password',
-          html: `This is the Token id: ${createEmailDto.tokenId}`,
-        });
+      await this.mailerService.sendMail({
+        to: createEmailDto.emailAddress,
+        subject: 'Reset Password',
+        html: `This is the Token id: ${createEmailDto.tokenId}`,
+      });
 
-        return {
-          statusCode: 200,
-          message: 'Token id is send to your email, you have 1 hour to activate it',
-          data: createEmailDto,
-        };
+      return {
+        statusCode: 200,
+        message:
+          'Token id is send to your email, you have 1 hour to activate it',
+        data: createEmailDto,
+      };
     } catch (err) {
-        throw new HttpException(
-          'Failed to send reset password email',
-          HttpStatus.BAD_REQUEST,
-        );
-    } 
+      throw new HttpException(
+        'Failed to send reset password email',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
-    
-  }
-
+}

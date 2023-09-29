0f374ab5bd35b204fb60a39e65c49ceb4ac2e149
Lukas Lee <jiawei3e1@gmail.com>
2023-09-29 08:16:44 +0800

    Able to data initialize an admin account now, can refer to it to do your data initialization

--- src/administrator/admin.module.ts
+++ src/administrator/admin.module.ts
@@ -3,10 +3,11 @@ import { AdministratorService } from './admin.service';
 import { AdministratorController } from './admin.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Administrator } from '../entities/administrator.entity';
+import { InitAdminService } from './init-admin.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Administrator])],
   controllers: [AdministratorController],
-  providers: [AdministratorService],
+  providers: [AdministratorService, InitAdminService],
 })
 export class AdministratorModule {}

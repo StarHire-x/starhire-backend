867b1b49636e19933ac062c7a6d03ba33f7ee58e
Alwin Ng Jun Wei <alwinngjw@gmail.com>
2023-09-29 12:56:41 +0800

    changed query for job applications to retrieve documents as well

--- src/job-application/job-application.service.ts
+++ src/job-application/job-application.service.ts
@@ -251,7 +251,7 @@ export class JobApplicationService {
     try {
       const jobApplication = await this.jobApplicationRepository.findOne({
         where: { jobApplicationId: jobAppliationId },
-        relations: ['jobSeeker', 'recruiter'],
+        relations: ['jobSeeker', 'recruiter', 'documents'],
       });
 
       if (jobApplication) {

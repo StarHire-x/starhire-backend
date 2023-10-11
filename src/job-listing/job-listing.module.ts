058a384d9355568d0c55c367073fb62286d4132a
Deshun99 <e0560014@u.nus.edu>
2023-10-11 22:47:40 +0800

    Job Application, Job Listing notification done

--- src/job-listing/job-listing.module.ts
+++ src/job-listing/job-listing.module.ts
@@ -8,6 +8,9 @@ import { JobApplication } from 'src/entities/jobApplication.entity';
 import { JobSeeker } from 'src/entities/jobSeeker.entity';
 import { Recruiter } from 'src/entities/recruiter.entity';
 import { JobAssignment } from 'src/entities/jobAssignment.entity';
+import { TwilioService } from 'src/twilio/twilio.service';
+import { EmailService } from 'src/email/email.service';
+import { Administrator } from 'src/entities/administrator.entity';
 
 @Module({
   imports: [
@@ -17,11 +20,12 @@ import { JobAssignment } from 'src/entities/jobAssignment.entity';
       JobApplication,
       JobSeeker,
       Recruiter,
-      JobAssignment
+      JobAssignment,
+      Administrator
     ]),
   ],
   controllers: [JobListingController],
-  providers: [JobListingService],
+  providers: [JobListingService,TwilioService,EmailService],
   exports: [TypeOrmModule.forFeature([JobListing])],
 })
 export class JobListingModule {}

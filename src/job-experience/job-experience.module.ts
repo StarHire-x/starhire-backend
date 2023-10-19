f4f43ec5dafc3cf4a4f4e49a609a9ece9cd31079
Deshun99 <e0560014@u.nus.edu>
2023-10-19 09:47:29 +0800

    similarity score logic is nested in backend

--- src/job-experience/job-experience.module.ts
+++ src/job-experience/job-experience.module.ts
@@ -7,9 +7,11 @@ import { JobSeeker } from 'src/entities/jobSeeker.entity';
 import { JobSeekerService } from 'src/job-seeker/job-seeker.service';
 import { EmailModule } from 'src/email/email.module';
 import { TwilioModule } from 'src/twilio/twilio.module';
+import { Corporate } from 'src/entities/corporate.entity';
+import { JobListing } from 'src/entities/jobListing.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([JobExperience, JobSeeker]), EmailModule, TwilioModule],
+  imports: [TypeOrmModule.forFeature([JobExperience, JobSeeker, Corporate, JobListing]), EmailModule, TwilioModule],
   controllers: [JobExperienceController],
   providers: [JobExperienceService, JobSeekerService],
 })

d853a63a9b67961c8ac56931d105eb661acfea4f
hengyjj <hengyjj@hotmail.com>
2023-10-24 08:11:31 +0800

    added administrator into commission.module.ts

--- src/commission/commission.module.ts
+++ src/commission/commission.module.ts
@@ -6,10 +6,11 @@ import { Commission } from 'src/entities/commission.entity';
 import { Recruiter } from 'src/entities/recruiter.entity';
 import { Invoice } from 'src/entities/invoice.entity';
 import { JobApplication } from 'src/entities/jobApplication.entity';
+import { Administrator } from 'src/entities/administrator.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Commission, Invoice, Recruiter, JobApplication]),
+    TypeOrmModule.forFeature([Commission, Invoice, Recruiter, JobApplication, Administrator]),
   ],
   controllers: [CommissionController],
   providers: [CommissionService],

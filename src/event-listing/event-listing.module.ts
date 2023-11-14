be1905c4971e4acbe1bf53cb7b1a343d1e4b1460
Shawn Lim <e0727020@u.nus.edu>
2023-11-14 11:47:47 +0800

    added EventRegistration to event listing module

--- src/event-listing/event-listing.module.ts
+++ src/event-listing/event-listing.module.ts
@@ -4,11 +4,14 @@ import { EventListingController } from './event-listing.controller';
 import { EventListing } from '../entities/eventListing.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Corporate } from '../entities/corporate.entity';
+import { EventRegistration } from '../entities/eventRegistration.entity';
 import { EmailService } from '../email/email.service';
 import { TwilioService } from '../twilio/twilio.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([EventListing, Corporate])],
+  imports: [
+    TypeOrmModule.forFeature([EventListing, Corporate, EventRegistration]),
+  ],
   controllers: [EventListingController],
   providers: [EventListingService, EmailService, TwilioService],
 })
